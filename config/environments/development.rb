65267875ba3c0ee6ea33ca3a8545fc7a38af901f
rodruby <deigolopez@yahoo.com>
2015-06-21 04:34:29 +0000

    changed config development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   #Added per devise for production, needs to be changed to the production url
-  config.action_mailer.default_url_options = { host: 'picsshare.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'murmuring-savannah-5872.herokuapp.com' }
   
 end
